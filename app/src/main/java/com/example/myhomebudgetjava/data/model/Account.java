e667bc480953aee8604801d9e6804ae26c3c11db
Raf Lew <82097814+RafLew84@users.noreply.github.com>
2023-04-02 18:06:00 +0200

    add dummy data and data model

---
+++ app/src/main/java/com/example/myhomebudgetjava/data/model/Account.java
@@ -0,0 +1,31 @@
+package com.example.myhomebudgetjava.data.model;
+
+public class Account {
+    private final String name;
+    private final String number;
+    private final double amount;
+    private final int color;
+
+    public Account(String name, String number, double amount, int color) {
+        this.name = name;
+        this.number = number;
+        this.amount = amount;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public int getColor() {
+        return color;
+    }
+}
